9f1a888bee24ad26d29af62739731a5243193531
Rustem Muslimov <r.muslimov@gmail.com>
2014-09-23 16:17:42 +0400

    Fix keys lifetime

--- fasttrace/settings.py
+++ fasttrace/settings.py
@@ -9,7 +9,7 @@
 TRACE_TOOL_URL = "amqp://guest:guest@localhost:5672/fasttrace"
 MEMCACHED_URL = "127.0.0.1"
 
-CACHE_TIMEOUT = 10
+CACHE_TIMEOUT = 60 * 60 * 24 * 7
 
 REGISTERED_TAGS = (
     ("amadeus", "Amadeus"),
