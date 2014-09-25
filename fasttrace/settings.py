8017e0190122730adf77e8a4ae9b6edf32230834
Rustem Muslimov <r.muslimov@gmail.com>
2014-09-25 16:48:46 +0400

    Use os.environ from env

--- fasttrace/settings.py
+++ fasttrace/settings.py
@@ -6,8 +6,12 @@
 
 """
 
-TRACE_TOOL_URL = "amqp://guest:guest@localhost:5672/fasttrace"
-MEMCACHED_URL = "127.0.0.1"
+import os
+
+TRACE_TOOL_URL = os.environ.get("TRACE_TOOL_URL",
+                                "amqp://guest:guest@localhost:5672/fasttrace")
+
+MEMCACHED_URL = os.environ.get("MEMCACHED_URL", "127.0.0.1")
 
 CACHE_TIMEOUT = 60 * 60 * 24 * 7
 
